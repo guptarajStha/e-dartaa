af0e71e27af80edc43fcacd34f3c9cc5904ee87c
guptarajStha <shresthaguptaraj@gmail.com>
2022-08-16 08:09:25 +0545

    edited

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'E-Darta',
       home: CheckPage(),
       routes: {
         MyRoutes.mainMenu: (context) => MainMenu(),
